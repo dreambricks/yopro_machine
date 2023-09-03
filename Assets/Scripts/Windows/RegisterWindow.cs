b86fdff883f085574b41812fc2ccfe9899b2cdd9
izanette <izanette@gmail.com>
2023-09-03 14:42:09 -0300

    checks on the telephone data

--- Assets/Scripts/Windows/RegisterWindow.cs
+++ Assets/Scripts/Windows/RegisterWindow.cs
@@ -31,6 +31,16 @@ public class RegisterWindow : MonoBehaviour
     private bool resultExists;
     private float currentTime;
 
+    private static int[] validDDDs = {
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 21,
+        22, 24, 27, 28, 31, 32, 33, 34, 35, 37,
+        38, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+        51, 53, 54, 55, 61, 62, 63, 64, 65, 66,
+        67, 68, 69, 71, 73, 74, 75, 77, 79, 81,
+        82, 83, 84, 85, 86, 87, 88, 89, 91, 92,
+        93, 94, 95, 96, 97, 98, 99
+    };
+
     private void Start()
     {
         advance.onClick.AddListener(()=> GetRegisterData());
@@ -170,11 +180,18 @@ public class RegisterWindow : MonoBehaviour
 
     public static bool IsPhoneValid(string phone)
     {
-        string pattern = @"^\(\d{2}\)\d{5}\-\d{4}$";
+        string pattern = @"^\(\d{2}\)[89]\d{4}\-\d{4}$";
 
-        bool isMatch = Regex.IsMatch(phone, pattern);
+        if (!Regex.IsMatch(phone, pattern)) return false;
 
-        return isMatch;
+        int ddd = int.Parse(phone.Substring(1, 2));
+
+        return IsDDDValid(ddd);
+    }
+
+    public static bool IsDDDValid(int ddd)
+    {
+        return validDDDs.Contains(ddd);
     }
 
     public static bool IsValidEmail(string email)
